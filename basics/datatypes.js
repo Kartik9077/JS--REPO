1888f0cd05f47b51cb742e2518bb3e05098c46c9
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-09-13 17:43:03 +0000

    new file

---
+++ basics/datatypes.js
@@ -0,0 +1,9 @@
+/*number 
+bigint
+string
+boolean 
+null
+undefined
+object
+symbol*/
+console.table([typeof "kartik",typeof 13,typeof null,typeof undefined,typeof false])
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,2 @@
 console.log("kartik")
+console.log("print kartik sharma is a good boy")
